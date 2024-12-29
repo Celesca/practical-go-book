ca26f721cc7ecb8ce4565106dfbd832e2058dbca
celesca <folk.sawit@gmail.com>
2024-12-30 02:00:08 +0700

    add: fetch remote resource test go

--- chap3/data-downloader/fetch_remote_resource_test.go
+++ chap3/data-downloader/fetch_remote_resource_test.go
@@ -1,5 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
 func startTestHTTPServer() *httptest.Server {
 	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Hello, client")
 	}))
+}
+
+func TestFetchRemoteResource(t *testing.T) {
+	server := startTestHTTPServer()
+	defer server.Close()
+
+	body, err := fetchRemoteResource(server.URL)
+	if err != nil {
+		t.Fatalf("Error fetching resource: %v", err)
+	}
+
+	if string(body) != "Hello, client\n" {
+		t.Fatalf("Unexpected response: %s", body)
+	}
 }
