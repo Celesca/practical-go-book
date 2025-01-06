88217bfd05b70a767d4f2a94af96258416284128
celesca <folk.sawit@gmail.com>
2025-01-07 01:37:58 +0700

    add: streaming-decode

---
+++ chap5/streaming-decode/server.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type logLine struct {
+	UserIP string `json:"user_ip"`
+	Event  string `json:"event"`
+}
+
+func decodeHandler(w http.ResponseWriter, r *http.Request) {
+	dec := json.NewDecoder(r.Body)
+
+	for {
+		var l logLine
+		err := dec.Decode(&l)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		fmt.Println(l.UserIP, l.Event)
+	}
+	fmt.Fprintln(w, "OK")
+}
